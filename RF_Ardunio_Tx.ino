37e21bedf439120abd1a52540455949801988616
mehakgarg573 <103985684+mehakgarg573@users.noreply.github.com>
2022-06-06 18:39:29 +0530

    Update RF_Ardunio_Tx.ino

--- RF_Ardunio_Tx.ino
+++ RF_Ardunio_Tx.ino
@@ -1,13 +1,13 @@
 #include <VirtualWire.h>
-char t;
+char m;
 
 char *data;
 
 void setup() 
 {
-  pinMode(5,OUTPUT);   //left motors forward
+  pinMode(5,OUTPUT);  //left motors forward
 pinMode(6,OUTPUT);   //left motors reverse
-pinMode(9,OUTPUT);   //right motors forward
+pinMode(10,OUTPUT);   //right motors forward
 pinMode(11,OUTPUT);   //right motors reverse 
 Serial.begin(9600);
 
@@ -15,58 +15,56 @@ Serial.begin(9600);
   vw_setup(2000);
   pinMode (5, LOW);
   pinMode (6, LOW);
-  pinMode (9, LOW);
+  pinMode (10, LOW);
   pinMode (11, LOW); 
-
- 
 }
+
 void loop()
 {
    if(Serial.available()){ 
-  t = Serial.read();
-  Serial.println(t);
+  m = Serial.read();
+  Serial.println(m);
 }
-  if(t == 'F')
+  if(m == 'F')
   {
     data="f";
     vw_send((uint8_t *)data, strlen(data));
-    vw_wait_tx();    
-    digitalWrite(9,HIGH);
+    vw_wait_tx();   
+    digitalWrite(10,HIGH);
       digitalWrite(5,HIGH);
   }
-  else if(t =='B')
+  else if(m =='B')
   {
     data="b";
     vw_send((uint8_t *)data, strlen(data));
     vw_wait_tx();
     digitalWrite(6,HIGH);
     digitalWrite(11,HIGH);
-
   }
-  else if(t == 'L'){
+  
+  else if(m == 'L'){
     
     data="l";
     vw_send((uint8_t *)data, strlen(data));
     vw_wait_tx();      
-  digitalWrite(9,HIGH);//turn left (right side motors rotate in forward direction, left side motors doesn't rotate)
+  digitalWrite(10,HIGH);    //turn left (right side motors rotate in forward direction, left side motors doesn't rotate)
 }
  
-else if(t == 'R'){
+else if(m == 'R'){
   
     data="r";
     vw_send((uint8_t *)data, strlen(data));
-    vw_wait_tx();//turn right (left side motors rotate in forward direction, right side motors doesn't rotate)
+    vw_wait_tx();     //turn right (left side motors rotate in forward direction, right side motors doesn't rotate)
   digitalWrite(5,HIGH);
 }
 
-
  
-else if(t == 'S'){
+else if(m == 'S'){
   
     data="s";
     vw_send((uint8_t *)data, strlen(data));
-    vw_wait_tx();//STOP (all motors stop)
-  digitalWrite(9,LOW);
+    vw_wait_tx();    //STOP (all motors stop)
+  digitalWrite(10,LOW);
   digitalWrite(6,LOW);
   digitalWrite(5,LOW);
   digitalWrite(11,LOW);
